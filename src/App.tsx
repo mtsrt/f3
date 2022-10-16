65950ba23989b1ef3a48ded94856f2ca66eb650f
matanosorata <matanosorata@SaMacBook-Pro.local>
2022-10-16 15:20:15 +0900

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./component/Header";
+import Home from "./component/Home";
+import User from "./component/User";
+import Entrance from "./component/Entrance";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/entrance" element={<Entrance />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div style={{ backgroundColor: "#DC143C", color: "white", height: "70px" }}>
+      <h1 style={{ marginLeft: "20px" }}>Test</h1>
+    </div>
+  );
+}
+
+export default Header;
