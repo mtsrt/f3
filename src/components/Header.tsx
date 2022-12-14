c8a98b93c01f702348611b066cc2938d911ba598
matanosorata <matanosorata@SaMacBook-Pro.local>
2022-12-14 23:45:46 +0900

    add: theme.ts fix: Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,21 @@
-import { Box, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Box, Link } from "@mui/material";
 
-const Header = () => {
-  const navigate = useNavigate();
-
+const Header: React.FC = () => {
   return (
     <Box sx={{ backgroundColor: "#424242", color: "white", height: "70px" }}>
       <Box fontSize={65} fontWeight="fontWeightBold" sx={{ letterSpacing: 10 }}>
         GateWay
+        <Link
+          component="button"
+          underline="none"
+          variant="body2"
+          onClick={() => {
+            console.info("I'm a button.");
+          }}
+        >
+          Button Link
+        </Link>
       </Box>
-      <Button onClick={() => navigate("/home")}>Home</Button>
     </Box>
   );
 };
